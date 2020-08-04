185389abba0f6e1de2e77802662aa3e442c0bdc0
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-04 17:17:12 +0100

    Wrote working unit test to check dock_bike method

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -3,6 +3,9 @@ class DockingStation
   def release_bike
     Bike.new
   end
+  def dock_bike(docking_station_name)
+    'free'
+  end
 end
 
 class Bike
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -3,7 +3,13 @@ describe DockingStation do
   it ' Expect DockingStation instances to respond to release_bike' do
     expect(DockingStation.new).to respond_to(:release_bike)
   end
-  it 'Check if the bike is working' do
+  it 'Expect true if bike is working' do
     expect(DockingStation.new.release_bike.working?).to eq true
+  end
+  it 'Expect DockingStation instances to respond to dock_bike' do
+    expect(DockingStation.new).to respond_to(:dock_bike)
+  end
+  it 'Expect docking station to be "free" when passed "docking station name"' do
+    expect(DockingStation.new.dock_bike('docking station name')).to eq 'free'
   end
   end

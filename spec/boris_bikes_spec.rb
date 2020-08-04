0ed6f1b2115b0082b61ad000e75bce1ed76eb59b
joshSteadman <josh.steadman15@gmail.com>
2020-08-04 16:45:58 +0100

    Test to check that we can get a working bike

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,6 +1,9 @@
 require 'boris_bikes'
 describe DockingStation do
-  it ' Expect docking_station to respond to release_bike' do
+  it ' Expect DockingStation instances to respond to release_bike' do
     expect(DockingStation.new).to respond_to(:release_bike)
   end
-end
+  it 'Check if the bike is working' do
+    expect(DockingStation.new.release_bike.working?).to eq true
+  end
+  end

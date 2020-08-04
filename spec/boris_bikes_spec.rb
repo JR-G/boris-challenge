349f535519015025d0b600f9289e5e63deadbdb9
joshSteadman <josh.steadman15@gmail.com>
2020-08-04 16:06:47 +0100

    Made and Passed second Unit Test

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,4 +1,6 @@
 require 'boris_bikes'
 describe DockingStation do
-
+  it ' Expect docking_station to respond to release_bike' do
+    expect(DockingStation.new).to respond_to(:release_bike)
+  end
 end
